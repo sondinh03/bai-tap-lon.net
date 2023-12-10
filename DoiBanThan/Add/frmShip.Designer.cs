2065d1da0360089d0bb82141888aed1aad7ec668
Đỗ Quý Mùi <quymui0608@gmail.com>
2023-12-10 22:33:01 +0700

    oske

--- DoiBanThan/Add/frmShip.Designer.cs
+++ DoiBanThan/Add/frmShip.Designer.cs
@@ -220,6 +220,7 @@
             this.Controls.Add(this.guna2Panel2);
             this.Controls.Add(this.guna2Panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmShip";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmShip";
